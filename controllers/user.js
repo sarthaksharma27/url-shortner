bcc609b70df22fed6a4c594c208ba87e33d7204d
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-01-04 07:48:52 +0530

    minor fix

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 const User = require("../models/user")
-const {setUser, getUser} = require("../service/auth")
+const {setUser} = require("../service/auth")
 
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
